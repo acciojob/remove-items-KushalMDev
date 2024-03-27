b9bc26a4830efa8314f7ad513ed78b8a70a2f1d7
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-27 14:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function remove() {
+    let selectElement = document.getElementById("colorSelect")
+    const selectedIndex = selectElement.selectedIndex;
+
+    // Check if an option is selected
+    if (selectedIndex !== -1) {
+      // Remove the selected option from the list
+      selectElement.remove(selectedIndex);
+}}
